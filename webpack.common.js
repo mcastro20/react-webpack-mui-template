0e940db751b30fbdec81b2cdc8b1f5ff6c19f57d
mcastro20 <michaelanthony.castro26@gmail.com>
2019-07-25 08:24:20 +0800

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname,'src','index.js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "media"
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
+  }
+};
